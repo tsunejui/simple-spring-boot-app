53581ed1955f61dfda7bb6a3693621f5fddc77b4
Rex <tsunejui@gmail.com>
2023-11-17 13:59:47 +0800

    remove the codes for restrict accessing

--- src/main/java/com/example/actuatordemo/config/ActuatorSecurityConfig.java
+++ src/main/java/com/example/actuatordemo/config/ActuatorSecurityConfig.java
@@ -25,19 +25,5 @@ public class ActuatorSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        // http
-        //         .authorizeRequests()
-        //             .requestMatchers(EndpointRequest.to(ShutdownEndpoint.class))
-        //                 .hasRole("ACTUATOR_ADMIN")
-        //             .requestMatchers(EndpointRequest.toAnyEndpoint())
-        //                 .permitAll()
-        //             .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
-        //                 .permitAll()
-        //             .antMatchers("/", "/slowApi")
-        //                 .permitAll()
-        //             .antMatchers("/**")
-        //                 .authenticated()
-        //         .and()
-        //         .httpBasic();
     }
 }
