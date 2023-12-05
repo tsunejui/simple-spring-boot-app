8ae4ba173f242ce10ffb4983ba909e228dc98e6f
gene <5656nhg8@gmail.com>
2023-12-05 14:49:24 +0800

    Update java 17

--- src/main/java/com/example/actuatordemo/config/ActuatorSecurityConfig.java
+++ src/main/java/com/example/actuatordemo/config/ActuatorSecurityConfig.java
@@ -1,29 +1,35 @@
 package com.example.actuatordemo.config;
 
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
-import org.springframework.boot.actuate.context.ShutdownEndpoint;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-public class ActuatorSecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration(proxyBeanMethods = false)
+public class ActuatorSecurityConfig {
 
     /*
-        This spring security configuration does the following
-
-        1. Restrict access to the Shutdown endpoint to the ACTUATOR_ADMIN role.
-        2. Allow access to all other actuator endpoints.
-        3. Allow access to static resources.
-        4. Allow access to the home page (/).
-        5. All other requests need to be authenticated.
-        5. Enable http basic authentication to make the configuration complete.
-           You are free to use any other form of authentication.
+     * This spring security configuration does the following
+     * 
+     * 1. Restrict access to the Shutdown endpoint to the ACTUATOR_ADMIN role.
+     * 2. Allow access to all other actuator endpoints.
+     * 3. Allow access to static resources.
+     * 4. Allow access to the home page (/).
+     * 5. All other requests need to be authenticated.
+     * 5. Enable http basic authentication to make the configuration complete.
+     * You are free to use any other form of authentication.
      */
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.securityMatcher(EndpointRequest.toAnyEndpoint());
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
         http.csrf().disable();
+        return http.build();
     }
+    // @Override
+    // protected void configure(HttpSecurity http) throws Exception {
+    // http.csrf().disable();
+    // }
 }
--- src/main/java/com/example/actuatordemo/health/CustomHealthIndicator.java
+++ src/main/java/com/example/actuatordemo/health/CustomHealthIndicator.java
@@ -1,8 +1,6 @@
 // src: https://docs.spring.io/spring-boot/docs/current/reference/html/actuator.html
 package com.example.actuatordemo.health;
 
-import java.util.Random;
-
 import org.springframework.boot.actuate.health.AbstractHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.stereotype.Component;
@@ -19,19 +17,21 @@ public class CustomHealthIndicator extends AbstractHealthIndicator {
         // Use the builder to build the health status details that should be reported.
         // If you throw an exception, the status will be DOWN with the exception
         // message.
-        int number = getRandomNumber();
 
-        if (number > 5) {
-            builder.up().withDetail("app", "hello world");
-        } else {
-            builder.down().withDetail("error", "Number was less than 5").build();
-            Thread.sleep(10 * 1000);
-            throw new Exception("Number was less than 5");
-        }
+        builder.up().withDetail("app", "hello world");
+        // int number = getRandomNumber();
+
+        // if (number > 5) {
+        // builder.up().withDetail("app", "hello world");
+        // } else {
+        // builder.down().withDetail("error", "Number was less than 5").build();
+        // Thread.sleep(10 * 1000);
+        // throw new Exception("Number was less than 5");
+        // }
     }
 
-    private int getRandomNumber() {
-        return new Random().ints(1, 10).findFirst()
-                .orElseThrow(() -> new RuntimeException("Failed to generate random number"));
-    }
+    // private int getRandomNumber() {
+    // return new Random().ints(1, 10).findFirst()
+    // .orElseThrow(() -> new RuntimeException("Failed to generate random number"));
+    // }
 }
